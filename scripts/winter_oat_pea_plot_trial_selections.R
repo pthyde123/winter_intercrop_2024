bc05cced2f619df8ccab2b6355798b1a7933c6f4
pthyde123 <pth7@cornell.edu>
2024-09-09 13:14:17 -0400

    updated blup selection index

--- scripts/winter_oat_pea_plot_trial_selections.R
+++ scripts/winter_oat_pea_plot_trial_selections.R
@@ -9,7 +9,8 @@ library(readxl)
 Cornell_WinterOatFounders_2024_Headrow_phenotypes <- read_excel("data/Cornell_WinterOatFounders_2024_Headrow_phenotypes.xlsx")
 
 
-OatTraitBLUPs <- read_excel("data/OatTraitBLUPs.xlsx")
+OatTraitBLUPs <-read_csv("data/selind_ListOfAccessionsPlantFall2024.csv")
+
 
 
 winter_2024_accession_max_seedlots <- read_csv("output/winter_2024_accession_max_seedlots.csv")
@@ -17,17 +18,19 @@ winter_2024_accession_max_seedlots <- read_csv("output/winter_2024_accession_max
 
 #####  Select top 80% by SelIndex
 
-keepers = round(nrow(OatTraitBLUPs)*.8)  ## number to keep, dropped from .8 to .79 to get final to 90 plots, even number.
+keepers = round(nrow(OatTraitBLUPs)*.77)  ## number to keep, dropped from .8 to .77 to get final to 72 plots, even number.
+
+
 
 
 plot_trial <- OatTraitBLUPs %>% 
   mutate("germplasmName" = Oat) %>% 
-  select(germplasmName,SelIndex) %>% 
-  arrange(SelIndex) %>% 
-  slice_max(SelIndex, n = keepers) %>% 
+  select(germplasmName,selIndex) %>% 
+  arrange(selIndex) %>% 
+  slice_max(selIndex, n = keepers) %>% 
   mutate("plots_to_plant" = 4) %>% 
   left_join(winter_2024_accession_max_seedlots,join_by(germplasmName)) %>% ### add in seed lot weights
-  select(observationUnitName,germplasmName,`Grain weight - g|CO_350:0005123`,plots_to_plant,SelIndex) %>% 
+  select(observationUnitName,germplasmName,`Grain weight - g|CO_350:0005123`,plots_to_plant,selIndex) %>% 
   print( n = nrow(OatTraitBLUPs))
 
 
